08b7e5bc6b3e8fde8333cc91cda56cf53c32a422
Joshua Schoenfeld <joshuaschoenfeld@Joshuas-MacBook-Air.local>
2017-11-20 13:35:39 -0500

    added some console logs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log("Up and stunning!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
