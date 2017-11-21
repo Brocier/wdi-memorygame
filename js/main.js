26fa2ca9b2edc0654229979a013dd5a8d489a5dc
Joshua Schoenfeld <joshuaschoenfeld@Joshuas-MacBook-Air.local>
2017-11-21 00:09:21 -0500

    created game logic to check for matching cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+//initial console logs to know that JS is working
 console.log("Up and stunning!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//array with my cards in it
+var cards = ["queen", "queen", "king", "king"]; 
+//empty array that is essential for the game logic
+var cardsInPlay = []
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}else {
+		alert("Sorry, try again.")
+	}
+}
+
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
