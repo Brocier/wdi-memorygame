1cc412bda2f5692096b56fd907c035c409da4b22
Joshua Schoenfeld <joshuaschoenfeld@Joshuas-MacBook-Air.local>
2017-11-22 23:49:13 -0500

    added functions to the JS file.

--- js/main.js
+++ js/main.js
@@ -4,20 +4,20 @@ console.log("Up and stunning!");
 var cards = ["queen", "queen", "king", "king"]; 
 //empty array that is essential for the game logic
 var cardsInPlay = []
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+//function to check for match
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	}else {
+		} else {
 		alert("Sorry, try again.")
-	}
-}
+}}};
+//function to simulate a user flipping a card
+var flipCard = function (cardId) {
+	console.log ("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
