e7439622c2708125df94424eb7d73d93a2cd7020
Joshua Schoenfeld <joshuaschoenfeld@Joshuas-MacBook-Air.local>
2017-11-26 00:30:50 -0500

    cards flipping and matching thanks to DOM

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 //initial console logs to know that JS is working
-console.log("Up and stunning!");
+console.log("JS working!");
 //array with my cards in it
+/*
+I want to be able to make this a lot "dry"er and include all 52 cards.
+perhaps making an array for rank and suit seperate from cards
+Then I can just have the cardImage concatenate with references to those arrays.
+e.g. cardImage: "images/" + rank + "-of-" + suit + ".png"
+*/
 var cards = [
  {
  	rank: "queen",
@@ -23,25 +29,26 @@ var cards = [
  	cardImage: "images/king-of-diamonds.png"
  }]; 
 //empty array that is essential for the game logic
-var cardsInPlay = []
+var cardsInPlay = [];
 //function to check for match
-var checkForMatch = function(){
-	this.setAttribute('src', cards.cardImage);
+var checkForMatch = function(clickedCard, cardId){
+	clickedCard.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 		} else {
-		console.log("Sorry, try again.")
+		alert("Sorry, try again.")
 }}};
 //function to simulate a user flipping a card
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
-	console.log ("User flipped" + cards[cardId].rank);
+	console.log ("User flipped " + cards[cardId].rank);
 	console.log (cards[cardId].cardImage);
 	console.log (cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	checkForMatch(this, cardId);
 };
+// function that creates the card elements, assigns them images, id's, and puts a click function on them.
 var createBoard = function(){
 	for (var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
